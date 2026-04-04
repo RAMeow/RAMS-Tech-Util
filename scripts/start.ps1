4f513943760c2498ed74c47b14d3a276dc139e5b
RAMeow <86502704+RAMeow@users.noreply.github.com>
2026-04-04 01:24:44 -0500

    Update start.ps1

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -51,7 +51,6 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     $script = if ($PSCommandPath) {
         "& { & `'$($PSCommandPath)`' $($argList -join ' ') }"
     } else {
-        "&([ScriptBlock]::Create((irm https://github.com/ChrisTitusTech/winutil/releases/latest/download/winutil.ps1))) $($argList -join ' ')"
     }
 
     $powershellCmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
