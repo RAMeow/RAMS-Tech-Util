254dc997d9047515d24bcb5fbd7295c5de1838e5
RAMeow <86502704+RAMeow@users.noreply.github.com>
2026-04-04 01:12:39 -0500

    Update sign.bat

--- sign.bat
+++ sign.bat
@@ -1,1 +1,1 @@
-signtool.exe sign /td sha256 /tr http://timestamp.digicert.com /fd sha256 /n "RAM'S COMPUTER REPAIR" .\Start-RAM-Tech-Utility.ps1
+signtool.exe sign /td sha256 /tr http://timestamp.digicert.com /fd sha256 /n "RAM'S COMPUTER REPAIR" .\RAM-Tech-Utility.exe
