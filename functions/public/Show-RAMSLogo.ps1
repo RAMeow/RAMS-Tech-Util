10af83689d4cbb7a73d8086e0f26db8dc1116e8a
RAMeow <86502704+RAMeow@users.noreply.github.com>
2026-04-04 03:20:37 -0500

    Update Show-RAMSLogo.ps1

--- functions/public/Show-RAMSLogo.ps1
+++ functions/public/Show-RAMSLogo.ps1
@@ -1,15 +1,15 @@
 function Show-RAMSLogo {
     Write-Host @"
-======================================================
+====================================================
 RRRRRR   AAAAAA   MM   MM  ''   SSSSS    
 R     R  A    A   MMM MMM   '  SS
-RRRRRR   AAAAAA   MM M MM      SSSSS      Tech Utility
+RRRRRR   AAAAAA   MM M MM      SSSSS   Tech Utility
 R   RR   A    A   MM   MM         SS
 R    RR  A    A   MM   MM    SSSSS
 
 RAM'S COMPUTER REPAIR
 Phone: (956) 244-5094
 Website: www.ramscomputerrepair.net
-=======================================================
+====================================================
 "@ -ForegroundColor Cyan
 }
